30a722a66ad908bdb5860e038ce087b787b6cdd6
David Harper <dave@libateq.org>
2021-05-11 17:46:22 +0100

    Remove brackets after import statement

--- account_uk_vat_analysis/tests/test_account_uk_vat_analysis.py
+++ account_uk_vat_analysis/tests/test_account_uk_vat_analysis.py
@@ -1,7 +1,7 @@
 # This file is part of the account_uk_vat_analysis Tryton module.
 # Please see the COPYRIGHT and README.rst files at the top level of this
 # package for full copyright notices, license terms and support information.
-from unittest import TestLoader()
+from unittest import TestLoader
 
 from trytond.tests.test_tryton import ModuleTestCase, suite as test_suite
 
