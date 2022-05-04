c02b4915ba51af003efcb2e8c30ec37d9dc37f5b
David Harper <dave@libateq.org>
2022-05-04 16:55:44 +0100

    Remove module account_uk_variants

--- account_uk_variants/doc/usage.rst
+++
@@ -1,7 +0,0 @@
-*****
-Usage
-*****
-
-Once this module has been activated you can use the
-`Chart Variants <model-account.chart_variant>` that it defines with the chart
-of accounts defined in the :doc:`Account UK Module <account_uk:index>`.
--- account_uk_variants/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file is part of the account_uk_variants Tryton module.
-# Please see the COPYRIGHT and README.rst files at the top level of this
-# package for full copyright notices, license terms and support information.
-try:
-    from trytond.modules.account_uk_variants.tests.test_account_uk_variants import suite  # noqa: E501
-except ImportError:
-    from .test_account_uk_variants import suite
-
-__all__ = ['suite']
