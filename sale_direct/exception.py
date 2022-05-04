cb51e12dc0ee7f8ecc7dc73d5f4eba7481ad5299
David Harper <dave@libateq.org>
2022-05-04 17:01:17 +0100

    Remove sale_direct module

--- sale_direct/exception.py
+++
@@ -1,8 +0,0 @@
-# This file is part of the booking Tryton module.
-# Please see the COPYRIGHT and README.rst files at the top level of this
-# package for full copyright notices, license terms and support information.
-from trytond.exceptions import UserError
-
-
-class MissingVisitLocationParentError(UserError):
-    "Raised when the visit location parent is needed but not configured"
